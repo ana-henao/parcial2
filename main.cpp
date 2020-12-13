48e292161c1ef3a800d2bcfd5af1c75ad822815a
Ana Henao <ana.henao8@udea.edu.co>
2020-12-12 23:48:55 -0500

    disparo ofensivo efectivo

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ void ImprimirResultados1(int angle, int V0o, float x, float y, int t)
 
 }
 
-Impacto* DisparoOfensivo(Do disparoO, Dd disparoD, int Voo, bool mostrar){
+Impacto DisparoOfensivo(Do disparoO, Dd disparoD, int Voo, bool mostrar){
     Impacto impactos[3];
     int flag=0;// contador de disparos
     float x,y;
@@ -67,6 +67,8 @@ Impacto* DisparoOfensivo(Do disparoO, Dd disparoD, int Voo, bool mostrar){
                     impactos[flag].x=x;
                     impactos[flag].y=y;
                     impactos[flag].t=t;
+                    //cout << angle<<endl;
+                    //cout << "Impacto angulo: "<<impactos[flag].angle<< endl;
                     if(mostrar) ImprimirResultados1(angle, V0o, x, y, t);
                     flag += 1;
                     V0o+=50;
@@ -85,7 +87,7 @@ Impacto* DisparoOfensivo(Do disparoO, Dd disparoD, int Voo, bool mostrar){
         cout << "No impacto en los disparos esperados"<< endl;
 
     }
-    return impactos;
+    return impactos[0];
 }
 
 void DisparoDefensivo(Do disparoO,Dd disparoD, int Voo){
@@ -127,7 +129,7 @@ void DisparoDefensivo(Do disparoO,Dd disparoD, int Voo){
     }
 }
 
-Impacto* DisparoDefensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vooo, bool mostrar){//dispara solo 2 seg despues del disparo ofensivo
+Impacto DisparoDefensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vooo, bool mostrar){//dispara solo 2 seg despues del disparo ofensivo
     Impacto impactos[3];
     int flag=0;
     float x,y, x2, y2;
@@ -138,9 +140,9 @@ Impacto* DisparoDefensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vo
     Vxoo=Vooo*cos((angleoo)*pi/180);
     Vyoo = Vooo*sin((angleoo)*pi/180);
     for(V0o = Voo; ; V0o += 5){
-        for(angle = 0; angle < 90; angle++){
-            Vxo = V0o*cos((angle+90)*pi/180);
-            Vy0 = V0o*sin((angle+90)*pi/180);
+        for(angle = 180; angle > 90; angle--){
+            Vxo = V0o*cos((angle)*pi/180);
+            Vy0 = V0o*sin((angle)*pi/180);
             x = 0.0;
             y = 0.0;
             x2 = 0.0;
@@ -153,10 +155,12 @@ Impacto* DisparoDefensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vo
                 if(sqrt(pow((x2 - x),2)+pow((y2 - y),2)) < disparoO.d0){
                     if(y<0) y=0;
                     impactos[flag].angle=angle;
-                    impactos[flag].V0o=Voo;
-                    impactos[flag].x=x2;
-                    impactos[flag].y=y2;
-                    impactos[flag].t=(t+2);
+                    impactos[flag].V0o=V0o;
+                    impactos[flag].x=x;
+                    impactos[flag].y=y;
+                    impactos[flag].t=(t);
+                    //cout << angle<<endl;
+                    //cout << "Impacto angulo: "<<impactos[flag].angle<< endl;
                     if(mostrar){
                         cout << "------------------ Normal: ---------------------------------"<< endl;
                         ImprimirResultados1(angleoo, Vooo, x2, y2, t+2);
@@ -181,78 +185,7 @@ Impacto* DisparoDefensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vo
         cout << "No impacto en los disparos esperados"<< endl;
 
     }
-    return impactos;
-}
-
-void DisparoOfensivoEfectivo (Do disparoO,Dd disparoD, int Voo){
-
-    Impacto* impactos=DisparoOfensivo(disparoO, disparoD, Voo, false);
-    Impacto impactoEfectivo=*impactos;
-//    int flag=0;//dice si imacta o no
-//    bool flag2=0;
-//    float x,y, x2, y2;
-//    float aux, auy;
-//    float Vxo,Vy0, Vxoo, Vyoo;
-//    int V0o = 0;
-//    int t = 0;
-//    int angle = 0;
-//    Vxoo=Vooo*cos((angleoo)*pi/180);
-//    Vyoo = Vooo*sin((angleoo)*pi/180);
-//    for(V0o = Voo; ; V0o += 5){
-//        for(angle = 180; angle > 90; angle--){
-//            Vxo = V0o*cos((angle)*pi/180);
-//            Vy0 = V0o*sin((angle)*pi/180);
-//            x = 0.0;
-//            y = 0.0;
-//            x2 = 0.0;
-//            y2 = 0.0;
-//            for(t = 0; ; t++){
-//                x2 = Vxoo*(t+2);
-//                y2 = disparoO.Yo + Vyoo*(t+2) -(0.5*G*(t+2)*(t+2));
-//                x = disparoD.Xd+Vxo*t;
-//                y = disparoD.Yd + Vy0*t -(0.5*G*t*t);
-//                for(int t2=t; ;t2++){//verifica defensivo, ofensivo no se ve afectado
-//                    aux = disparoD.Xd+Vxo*t2;
-//                    auy = disparoD.Yd + Vy0*t2 -(0.5*G*t*t);
-//                    if(sqrt(pow((disparoO.Xo - aux),2)+pow((disparoO.Yo - auy),2)) < disparoO.d0){
-
-//                        flag2 = 1;
-//                        break;
-//                    }
-//                    if(auy < 0){
-//                        break;
-//                    }
-//                }
-//                if (flag2){
-//                    flag2=0;
-//                    break;
-//                }
-//                if(sqrt(pow((disparoD.Xd - x2),2)+pow((disparoD.Yd - y2),2)) < disparoO.d0){
-//                    break;
-//                }
-//                if(sqrt(pow((x2 - x),2)+pow((y2 - y),2)) < disparoD.d0){
-//                    if(y<0) y = 0;
-
-//                    ImprimirResultados1(angleoo,Vooo,x2,y2,t+2);
-//                    cout << "_________________________________"<<endl;
-//                    ImprimirResultados1(angle, V0o, x, y, t);
-//                    flag += 1;
-//                    V0o += 50;
-//                    break;
-//                }
-//                if(y < 0){
-//                    break;
-//                }
-//            }
-//            if(flag == 3) break;
-
-//        }
-//        if(flag == 3) break;
-//    }
-//    if(flag != 3){
-//        cout << "No impacto en los disparos esperados"<< endl;
-//    }
-
+    return impactos[0];
 }
 
 void DisparoOfensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vooo){//dispara solo 2 seg despues del disparo ofensivo
@@ -262,8 +195,9 @@ void DisparoOfensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vooo){/
     int V0o = 0;//velocidad inicial de disparo con la que arranca el cañon
     int t = 0;
     int angle = 0;//no se coloca angulo especifico por que no se sabe a que angulo dispara para que le diera
-    Vxoo=Vooo*cos((angleoo)*pi/180);
-    Vyoo = Vooo*sin((angleoo)*pi/180);
+
+    Vxoo=Vooo*cos((90-(angleoo-90))*pi/180);
+    Vyoo = Vooo*sin((90-(angleoo-90))*pi/180);
     for(V0o = Voo; ; V0o += 5){
         for(angle = 0; angle < 90; angle++){
             Vxo = V0o*cos((angle)*pi/180);
@@ -304,6 +238,23 @@ void DisparoOfensivo2(Do disparoO,Dd disparoD, int Voo, int angleoo, int Vooo){/
     }
 }
 
+void DisparoOfensivoEfectivo (Do disparoO,Dd disparoD, int Voo){
+
+    Impacto impactoEfectivo=DisparoOfensivo(disparoO, disparoD, Voo, false);
+    cout <<"disparo ofensivo efectivo"<< endl;
+    ImprimirResultados1(impactoEfectivo.angle,impactoEfectivo.V0o,impactoEfectivo.x,impactoEfectivo.y,impactoEfectivo.t);
+    Impacto impactoDefensivo=DisparoDefensivo2(disparoO,disparoD,Voo,impactoEfectivo.angle,impactoEfectivo.V0o,false);
+    cout <<"disparo defensivo que compromete la efectividad del ataque ofensivo"<< endl;
+    ImprimirResultados1(impactoDefensivo.angle,impactoDefensivo.V0o,impactoDefensivo.x,impactoDefensivo.y,impactoDefensivo.t);
+    cout <<"disparos que neutralizan el ataque defensivo" <<endl;
+    DisparoOfensivo2(disparoO,disparoD, Voo,impactoDefensivo.angle,impactoDefensivo.V0o);
+
+
+
+}
+
+
+
 int main()
 {
     Do disparoO;
@@ -315,7 +266,7 @@ int main()
     //DisparoDefensivo(disparoO, disparoD, Voo);
     //DisparoDefensivo2(disparoO,disparoD,Voo,31,85);
     //DisparoDefensivo3(disparoO,disparoD,Voo,31,85);
-    //DisparoOfensivo2(disparoO, disparoD, Voo, 31,85);
+    //DisparoOfensivo2(disparoO, disparoD, Voo, 93,85);
     DisparoOfensivoEfectivo ( disparoO, disparoD,Voo);
 
 

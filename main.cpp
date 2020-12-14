70f65a8f14717bee57ed307c4517ee4316dee8ef
Ana Henao <ana.henao8@udea.edu.co>
2020-12-13 20:02:03 -0500

    MENU OPCIONES Y CODIGO FINAL

--- main.cpp
+++ main.cpp
@@ -25,18 +25,46 @@ void DisparoOfensivoEfectivo (int Voo){
 
 int main()
 {
-    int Voo;
+    int Voo,opcion;
+    cout<<"Simulador de disparos. "<<endl;
     cout <<"Ingrese Vo desde la cual quiere probar: "<<endl;
     cin >> Voo;
+    cout <<"Seleccione una opcion: "<<endl;
+    cout <<"1.disparos ofensivos que comprometen la integridad del canion defensivo. "<<endl;
+    cout <<"2.disparos defensivos que comprometen la integridad del canion ofensivo. "<<endl;
+    cout <<"3.disparos defensivos que impidan que el canion defensivo sea destruido "<<endl;
+    cout <<"4.disparos defensivos que impidan que los caniones defensivo y ofensivo puedan ser destruidos."<<endl;
+    cout <<"5.disparos que neutralicen el ataque defensivo y permitan que el ataque ofensivo sea efectivo."<<endl;
+    cin>>opcion;
     canionDefensivo disparoDefensivo=canionDefensivo();
     canionOfensivo disparoOfensivo=canionOfensivo();
-   // batalla_.DisparoOfensivo(disparoDefensivo, disparoOfensivo, Voo, true);
-    //batalla_.DisparoDefensivo(disparoDefensivo, disparoOfensivo, Voo);
-    //batalla_.DisparoDefensivo2(disparoDefensivo, disparoOfensivo,Voo,31,85,true);
-    //batalla_.DisparoOfensivo2(disparoDefensivo,disparoOfensivo, Voo, 84,109);
-    //canionDefensivo disparoDefensivo1=canionDefensivo();
-    //canionOfensivo disparoOfensivo1=canionOfensivo();
-    DisparoOfensivoEfectivo(Voo);
-
+    canionDefensivo disparoDefensivo1=canionDefensivo();
+    canionOfensivo disparoOfensivo1=canionOfensivo();
+    while(opcion <=5){
+        if (opcion==1){
+            batalla_.DisparoOfensivo(disparoDefensivo, disparoOfensivo, Voo, true);
+            break;
+        }
+        else if (opcion==2){
+            batalla_.DisparoDefensivo(disparoDefensivo, disparoOfensivo, Voo);
+            break;
+        }
+        else if(opcion==3){
+            batalla_.DisparoDefensivo2(disparoDefensivo, disparoOfensivo,Voo,31,85,true);
+            break;
+        }
+        else if(opcion==4){
+            batalla_.DisparoOfensivo2(disparoDefensivo,disparoOfensivo, Voo, 150,109);
+            //break;
+        }
+        else if(opcion==5){
+             DisparoOfensivoEfectivo(Voo);
+             break;
+        }
+        else{
+            cout <<"La opcion ingresada no existe."<< endl;
+        }
+    }
     return 0;
+
 }
